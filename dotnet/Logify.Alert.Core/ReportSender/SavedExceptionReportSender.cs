c166de09470079c327b44200560a7780067b1efa
Anton Mironov <tony@devexpress.com>
2017-01-20 17:35:44 +0300

    save report to temp dir send reports from temp dir

---
+++ dotnet/Logify.Alert.Core/ReportSender/SavedExceptionReportSender.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DevExpress.Logify.Core {
+    public class SavedExceptionReportSender {
+        public string DirectoryName { get; set; }
+        public IExceptionReportSender Sender { get; set; }
+
+        public void TrySendSavedReports() {
+            try {
+                if (!String.IsNullOrEmpty(DirectoryName))
+                    return;
+                if (Sender != null && Sender.CanSendExceptionReport())
+                    return;
+                if (!Directory.Exists(DirectoryName))
+                    return;
+
+                Thread thread = new Thread(TrySendSavedReportsWorker);
+                //thread.Priority = ThreadPriority.Highest;
+                thread.Start();
+            }
+            catch {
+            }
+        }
+
+        void TrySendSavedReportsWorker() {
+            try {
+                TrySendSavedReportsCore();
+            }
+            catch {
+            }
+        }
+
+        void TrySendSavedReportsCore() {
+            if (!String.IsNullOrEmpty(DirectoryName))
+                return;
+            if (Sender != null && Sender.CanSendExceptionReport())
+                return;
+            if (!Directory.Exists(DirectoryName))
+                return;
+
+            string[] fileNames = Directory.GetFiles(DirectoryName, TempDirectoryExceptionReportSender.TempFileNamePrefix + "*." + TempDirectoryExceptionReportSender.TempFileNameExtension);
+            foreach (string fileName in fileNames)
+                TrySendSavedReport(Path.Combine(DirectoryName, fileName));
+        }
+
+        void TrySendSavedReport(string fileName) {
+            try {
+                FileInfo info = new FileInfo(fileName);
+                if (info.Length > 1024 * 1024) { // file size if more than 1Mb
+                    File.Delete(fileName);
+                    return;
+                }
+
+                LogifyClientExceptionReport report = new LogifyClientExceptionReport();
+                StringBuilder content = new StringBuilder(File.ReadAllText(fileName));
+                report.ReportContent = content;
+                if (Sender.SendExceptionReport(report))
+                    File.Delete(fileName);
+            }
+            catch {
+            }
+        }
+    }
+}
---
+++ dotnet/Logify.Alert.Core/ReportSender/TempDirectoryExceptionReportSender.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DevExpress.Logify.Core {
+    public class TempDirectoryExceptionReportSender : ExceptionReportSenderSkeleton {
+        public const string TempFileNamePrefix = "LogifyR_";
+        public const string TempFileNameExtension = "bin";
+        public string DirectoryName { get; set; }
+        public Encoding Encoding { get; set; }
+
+        public override bool CanSendExceptionReport() {
+            return !String.IsNullOrEmpty(DirectoryName);
+        }
+
+        protected override bool SendExceptionReportCore(LogifyClientExceptionReport report) {
+            try {
+                if (!Directory.Exists(DirectoryName))
+                    Directory.CreateDirectory(DirectoryName);
+
+                if (!Directory.Exists(DirectoryName))
+                    return false;
+
+                string fileName = CreateTempFileName(DirectoryName);
+                if (String.IsNullOrEmpty(fileName))
+                    return false;
+
+                Encoding encoding = this.Encoding;
+                if (encoding == null)
+                    encoding = Encoding.UTF8;
+
+                File.WriteAllText(fileName, report.ReportString, encoding);
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+
+        string CreateTempFileName(string directoryName) {
+            DateTime now = DateTime.Now;
+            string fileNameTemplate = String.Format(TempFileNamePrefix + "{0}_{1}_{2}_{3}_{4}_{5}", now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            for (int i = 0; i < 100; i++) {
+                string fileName = fileNameTemplate;
+                if (i != 0)
+                    fileName += "_" + i.ToString();
+                fileName += "." + TempFileNameExtension;
+                string fullPath = Path.Combine(directoryName, fileName);
+                if (!File.Exists(fullPath))
+                    return fullPath;
+            }
+            return String.Empty;
+        }
+
+        public override IExceptionReportSender CreateEmptyClone() {
+            return new TempDirectoryExceptionReportSender();
+        }
+        public override void CopyFrom(IExceptionReportSender instance) {
+            base.CopyFrom(instance);
+            TempDirectoryExceptionReportSender other = instance as TempDirectoryExceptionReportSender;
+            if (other == null)
+                return;
+
+            this.DirectoryName = other.DirectoryName;
+            this.Encoding = other.Encoding;
+        }
+    }
+}
