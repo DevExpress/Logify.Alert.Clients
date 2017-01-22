928ba09e84694f3dd09096b1f6953e8119aa3958
Anton Mironov <tony@devexpress.com>
2017-01-22 21:29:11 +0300

    NLog target

---
+++ dotnet/Logify.Alert.NLog/LogifyAlertTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DevExpress.Logify.Core;
+using NLog.Targets;
+using NLog;
+
+namespace DevExpress.Logify.Alert.NLog {
+    [Target("LogifyAlert")]
+    public sealed class LogifyAlertTarget : TargetWithLayout {
+        protected override void Write(LogEventInfo logEvent) {
+            if (logEvent.Exception != null) {
+                if (LogifyClientBase.Instance != null)
+                    LogifyClientBase.Instance.Send(logEvent.Exception);
+            }
+        }
+    }
+}
