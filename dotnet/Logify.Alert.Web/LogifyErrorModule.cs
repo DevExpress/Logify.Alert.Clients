7866d392e2f9678d3c59bd6506f1bafb3997293f
Anton Mironov <tony@devexpress.com>
2017-02-08 17:39:56 +0300

    obsolete: DevExpress.Logify.Web.Register

--- dotnet/Logify.Alert.Web/LogifyErrorModule.cs
+++ dotnet/Logify.Alert.Web/LogifyErrorModule.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using DevExpress.Logify.Core;
 using System.Web.Configuration;
+using System.ComponentModel;
 
 namespace DevExpress.Logify.Web {
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Use the LogifyAlert.Instance.Send instead.")]
     public static class Register {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Browsable(false)]
+        [Obsolete("Use the LogifyAlert.Instance.Send instead.")]
         public static void Exception(Exception e) {
             LogifyAlert.Instance.Send(e);
         }
