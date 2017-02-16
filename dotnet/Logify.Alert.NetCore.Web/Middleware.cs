d40ba9a761b5cd8667c699046d16c3c382bf899d
Anton Mironov <tony@devexpress.com>
2017-02-16 14:00:05 +0300

    netcore mvc: wrap 404 with exception and send to logify

--- dotnet/Logify.Alert.NetCore.Web/Middleware.cs
+++ dotnet/Logify.Alert.NetCore.Web/Middleware.cs
@@ -15,18 +15,62 @@ namespace DevExpress.Logify.Web {
         }
 
         public async Task Invoke(HttpContext context) {
+            //context.Response.StatusCode
             try {
                 await next(context);
             }
             catch (Exception e) {
-                LogifyHttpContext.Current = context;
-                LogifyAlert.Instance.Send(e);
-                LogifyHttpContext.Current = null;
+                ReportException(e, context);
                 throw e;
+            }
+            if (context.Response != null && context.Response.StatusCode == 404) {
+                try {
+                    LogifyHttpException e = new LogifyHttpException(404, Create404ExceptionMessage(context));
+                    throw e;
+                }
+                catch (Exception ex) {
+                    ReportException(ex, context);
+                }
+            }
+        }
+        string Create404ExceptionMessage(HttpContext context) {
+            try {
+                if (context.Request == null || context.Request.Path == null)
+                    return String.Empty;
+                return String.Format("The controller for path '{0}' was not found or does not implement IController.", context.Request.Path.ToString());
+            }
+            catch {
+                return String.Empty;
+            }
+        }
+        void ReportException(Exception ex, HttpContext context) {
+            LogifyHttpContext.Current = context;
+            try {
+                LogifyAlert.Instance.Send(ex);
+            }
+            catch {
+                LogifyHttpContext.Current = null;
             }
         }
     }
 
+    class LogifyHttpException : Exception {
+        readonly int httpStatusCode;
+
+        public LogifyHttpException(int httpStatusCode) {
+            this.httpStatusCode = httpStatusCode;
+        }
+
+        public LogifyHttpException(int httpStatusCode, string message) : base(message) {
+            this.httpStatusCode = httpStatusCode;
+        }
+
+        public LogifyHttpException(int httpStatusCode, string message, Exception inner) : base(message, inner) {
+            this.httpStatusCode = httpStatusCode;
+        }
+
+        public int StatusCode { get { return this.httpStatusCode; } }
+    }
     public static class LogifyIAlertApplicationBuilderExtensions {
         public static IApplicationBuilder UseLogifyAlert(this IApplicationBuilder builder, Microsoft.Extensions.Configuration.IConfigurationSection config) {
             return builder.UseMiddleware<LogifyAlertMiddleware>(config);
