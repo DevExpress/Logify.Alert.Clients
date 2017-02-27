a313fe2733614c7e319f95968c638bcb4c96aa19
Anton Mironov <tony@devexpress.com>
2017-02-27 12:38:06 +0300

    update display collector

--- dotnet/Logify.Alert.Core/Collectors/DisplayCollector.cs
+++ dotnet/Logify.Alert.Core/Collectors/DisplayCollector.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Drawing;
 using System.Globalization;
+using System.Windows.Forms;
 
 namespace DevExpress.Logify.Core {
     public class DisplayCollector : IInfoCollector {
         public virtual void Process(Exception ex, ILogger logger) {
             //Size size = System.Windows.Forms.SystemInformation.WorkingArea.Size;
-            Size size = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+            Screen screen = Screen.PrimaryScreen;
+            Size size = screen.Bounds.Size;
             logger.BeginWriteObject("display");
             try {
                 logger.WriteValue("width", size.Width.ToString());
                 logger.WriteValue("height", size.Height.ToString());
+                logger.WriteValue("colorBits", screen.BitsPerPixel.ToString());
 
                 PointF dpi = GetDpi();
-                if (dpi.IsEmpty) {
+                if (!dpi.IsEmpty) {
                     logger.WriteValue("dpiX", dpi.X.ToString(CultureInfo.InvariantCulture));
                     logger.WriteValue("dpiY", dpi.Y.ToString(CultureInfo.InvariantCulture));
                 }
