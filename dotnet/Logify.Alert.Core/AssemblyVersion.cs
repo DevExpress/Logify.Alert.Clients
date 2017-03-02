3d4124bf185cc8d00acd849ecdf8e29af28a8756
Anton Mironov <tony@devexpress.com>
2017-03-02 13:55:12 +0300

    update version to 1.0.31

--- dotnet/Logify.Alert.Core/AssemblyVersion.cs
+++ dotnet/Logify.Alert.Core/AssemblyVersion.cs
@@ -2,7 +2,7 @@
 
 namespace DevExpress.Logify.Core {
     public static class AssemblyInfo {
-        public const string AssemblyCopyright = "Copyright (c) 2000-2016 Developer Express Inc.";
+        public const string AssemblyCopyright = "Copyright (c) 2000-2017 Developer Express Inc.";
         public const string AssemblyCompany = "Developer Express Inc.";
         public const string AssemblyProduct = "DevExpress Logify Alert";
         public const string AssemblyTrademark = "DevExpress Logify Alert";
@@ -18,7 +18,7 @@ namespace DevExpress.Logify.Core {
 #elif NETFX_CORE
     public const string Version = VersionShort + ".0.3";
 #else
-        public const string Version = VersionShort + ".30";
+        public const string Version = VersionShort + ".31";
 #endif
 
 
