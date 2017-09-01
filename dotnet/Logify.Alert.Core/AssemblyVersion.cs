dc0e10bd51408b1ad9a3f0e247802bbc979286ee
Anton Mironov <tony@devexpress.com>
2017-09-01 15:37:42 +0300

    update version to .35

--- dotnet/Logify.Alert.Core/AssemblyVersion.cs
+++ dotnet/Logify.Alert.Core/AssemblyVersion.cs
@@ -18,7 +18,7 @@ namespace DevExpress.Logify.Core {
 #elif NETFX_CORE
     public const string Version = VersionShort + ".0.3";
 #else
-        public const string Version = VersionShort + ".34";
+        public const string Version = VersionShort + ".35";
 #endif
 
 
