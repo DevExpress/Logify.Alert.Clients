8f0f0bb55e8666e96fd4817613de5fdd626ff8e0
Roman Kolesnikov <roman.kolesnikov@devexpress.com>
2018-05-08 12:50:42 +0300

    Logify.Alert.Js: Refactoring

--- js/Logify.Alert.JS/src/collectors/customDataCollector.js
+++ js/Logify.Alert.JS/src/collectors/customDataCollector.js
@@ -12,15 +12,12 @@ export default class customDataCollector extends collectorBase {
         if((this.owner == null) || (this.owner == undefined))
             return;
 
-        if(this.owner.customData != undefined)
-            report.customData = this.owner.customData;
-        if (additionalCustomData)    
-            report.customData = this.mergeCustomData(report.customData, additionalCustomData);   
+        report.customData = this.mergeCustomData(this.owner.customData, additionalCustomData);   
     }
     mergeCustomData(reportCustomData, additionalCustomData) {
+        if (!reportCustomData)
+            reportCustomData = {};
         if (typeof additionalCustomData === "object") {
-            if (!reportCustomData)
-                reportCustomData = {};
             return Object.assign({}, reportCustomData, additionalCustomData);    
         } else if (typeof additionalCustomData === "string" || typeof additionalCustomData === "number") {
             return Object.assign({}, reportCustomData, { additionalData: additionalCustomData });    
