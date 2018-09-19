16ade64bdfcd35eff76a4820da5e8238e00e9f55
Vladimir Gein <vladimir.gein@devexpress.com>
2018-09-19 18:04:11 +0300

    Xamarin.iOS: output path & AssemblyInfo.cs

--- dotnet/Logify.Alert.Xamarin.iOS.Unified/Properties/AssemblyInfo.cs
+++ dotnet/Logify.Alert.Xamarin.iOS.Unified/Properties/AssemblyInfo.cs
@@ -1,26 +1,38 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using DevExpress.Logify.Core.Internal;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("Logify.Alert.Xamarin.iOS")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DevExpress Logify Alert Xamarin.iOS Client")]
+[assembly: AssemblyDescription(AssemblyInfo.AssemblyDescriptionCommon +
+"The Logify.Alert.Xamarin.iOS package contains the Logify Alert client for Xamarin.Android applications. Use this client to set up your application so it reported uncaught exceptions to the service.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany(AssemblyInfo.AssemblyCompany)]
+[assembly: AssemblyProduct(AssemblyInfo.AssemblyProduct)]
+[assembly: AssemblyCopyright(AssemblyInfo.AssemblyCopyright)]
+[assembly: AssemblyTrademark(AssemblyInfo.AssemblyTrademark)]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: SatelliteContractVersion(AssemblyInfo.SatelliteContractVersion)]
+[assembly: NeutralResourcesLanguage("en-US")]
 
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.FileVersion)]
+[assembly: AssemblyInformationalVersion(AssemblyInfo.Version)]
