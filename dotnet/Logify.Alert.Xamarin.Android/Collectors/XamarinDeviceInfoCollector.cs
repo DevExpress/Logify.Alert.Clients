3b3bd1ab790c13b58266555b6c52eed1ec682734
Vladimir Gein <geinvp@gmail.com>
2018-10-12 14:42:11 +0300

    Added orientation information to Xamarin.Android client

--- dotnet/Logify.Alert.Xamarin.Android/Collectors/XamarinDeviceInfoCollector.cs
+++ dotnet/Logify.Alert.Xamarin.Android/Collectors/XamarinDeviceInfoCollector.cs
@@ -1,11 +1,7 @@
-using Android.App;
-using Android.Content;
 using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Runtime.ExceptionServices;
-using System.Threading;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 
 namespace DevExpress.Logify.Core.Internal {
     public class XamarinDeviceInfoCollector : IInfoCollector {
@@ -14,10 +10,24 @@ namespace DevExpress.Logify.Core.Internal {
             try {
                 logger.WriteValue("manufacturer", Android.OS.Build.Manufacturer);
                 logger.WriteValue("model", Android.OS.Build.Model);
+                logger.WriteValue("orientation", GetOrientation());
                 logger.WriteValue("product", Android.OS.Build.Product);
             }
             finally {
                 logger.EndWriteObject("device");
+            }
+        }
+
+        private string GetOrientation() {
+            try {
+                IWindowManager windowManager = Android.App.Application.Context.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+
+                SurfaceOrientation rotation = windowManager.DefaultDisplay.Rotation;
+                bool isLandscape = rotation == SurfaceOrientation.Rotation90 || rotation == SurfaceOrientation.Rotation270;
+                return isLandscape ? "Landscape" : "Portrait";
+            }
+            catch {
+                return "Unknown";
             }
         }
     }
