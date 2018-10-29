d9bcba7b2b28ee49c9202eb71b616bf668dc1483
Vladimir Gein <vladimir.gein@devexpress.com>
2018-10-29 17:31:51 +0300

    Fix publish_core.cmd script

--- dotnet/Logify.Alert.Core/publish_core.cmd
+++ dotnet/Logify.Alert.Core/publish_core.cmd
@@ -2,6 +2,6 @@
 rem nuget pack Logify.Alert.Core.csproj -Verbosity detailed -Prop Platform=AnyCPU -Prop Configuration=Release40
 rem nuget pack Logify.Alert.Core.csproj -Verbosity detailed -Prop Platform=AnyCPU -Prop Configuration=Release45
 rem nuget pack Logify.Alert.Core.NetCore.csproj -Verbosity detailed -Prop Platform=AnyCPU -Prop Configuration=Release
-dotnet restore Logify.Alert.Core.NetCore.csproj
-dotnet build Logify.Alert.Core.NetCore.csproj
-dotnet pack Logify.Alert.Core.NetCore.csproj
+dotnet restore Logify.Alert.NetCore.Core.csproj
+dotnet build Logify.Alert.NetCore.Core.csproj
+dotnet pack Logify.Alert.NetCore.Core.csproj
