fdc3119ed175a0ef6ea3ac09ef8610c449fecc2d
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2018-11-30 18:48:25 +0300

    improved ex searching, saved log message

--- dotnet/Logify.Alert.Log4Net/LogifyAlertAppender.cs
+++ dotnet/Logify.Alert.Log4Net/LogifyAlertAppender.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using DevExpress.Logify.Core;
 using log4net.Appender;
 using log4net.Core;
 
 namespace DevExpress.Logify.Alert.Log4Net {
     public class LogifyAppender : AppenderSkeleton {
-        public static void Init() {
-        }
-
+        public static void Init() { }
         protected override void Append(LoggingEvent loggingEvent) {
-            if (loggingEvent != null && loggingEvent.ExceptionObject != null) {
-                if (LogifyClientBase.Instance != null)
-                    LogifyClientBase.Instance.Send(loggingEvent.ExceptionObject);
+            if (loggingEvent != null) {
+                Exception exceptionObject = null;
+                IDictionary<string, string> customData = null;
+                if (loggingEvent.ExceptionObject != null)
+                    exceptionObject = loggingEvent.ExceptionObject;
+                if (loggingEvent.MessageObject != null) {
+                    if (exceptionObject == null)
+                        exceptionObject = loggingEvent.MessageObject as Exception;
+                    else
+                        customData = new Dictionary<string, string> {
+                            { "LoggingEventMessage", loggingEvent.MessageObject.ToString() }
+                        };
+                }
+                if (exceptionObject != null && LogifyClientBase.Instance != null)
+                    LogifyClientBase.Instance.Send(exceptionObject, customData);
             }
         }
     }
