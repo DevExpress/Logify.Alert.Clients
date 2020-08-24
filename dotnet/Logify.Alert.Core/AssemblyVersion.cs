f753d102d89531e30e7cb3a2de25265c7b09eee5
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2020-08-24 16:17:39 +0300

    version up to .55

--- dotnet/Logify.Alert.Core/AssemblyVersion.cs
+++ dotnet/Logify.Alert.Core/AssemblyVersion.cs
@@ -18,7 +18,7 @@ namespace DevExpress.Logify.Core.Internal {
 #elif NETFX_CORE
     public const string Version = VersionShort + ".0.3";
 #else
-        public const string Version = VersionShort + ".54";
+        public const string Version = VersionShort + ".55";
 #endif
 
 
